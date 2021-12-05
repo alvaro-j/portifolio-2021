8f3a6c6509b8f102d54abee56312b300faab75d0
alvaro-j <alvarojaime360@gmail.com>
2021-12-05 11:36:24 -0300

    add details tag and remove ul list from introduction

--- src/components/Tela1.js
+++ src/components/Tela1.js
@@ -1,29 +1,56 @@
 import React from "react";
 
 export default function Tela1() {
-  return (
-    <div>
-      <section className="introducao">
-        <article>
-          <h2>👋Hello world</h2>
-          <h1>Álvaro Jaime</h1>
-          <h2>Frontend Developer</h2>
-          <p>
-            Conhecimentos:
-          </p>
-          <ul>
-            <li>JavaScript, React</li>
-            <li>CSS, SASS</li>
-            <li>HTML</li>
-            <li>Git e Github</li>
-            <li>Inglês</li>
-          </ul>
-        </article>
-        <div className="introducao-img">
-          <img src="images/undraw_react_y7wq.svg" alt="svg react"></img>
-        </div>
-      </section>
-    </div>
-  );
+	return (
+		<div>
+			<section className="introducao">
+				<article>
+					<h2>👋Hello world</h2>
+					<h1>Álvaro Jaime</h1>
+					<h2>Frontend Developer</h2>
+					<p>Conhecimentos:</p>
+					<details>
+						<summary>Front End</summary>
+						<div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>JavaScript, React</p>
+						</div>
+						<div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>CSS, SASS</p>
+						</div>
+						<div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>HTML</p>
+						</div>
+					</details>
+					<details>
+						<summary>Ferramentas</summary>
+            <div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>Git e Github</p>
+						</div>
+            <div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>Visual Studio Code</p>
+						</div>          
+					</details>
+					<details>
+						<summary>Idiomas</summary>
+            <div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>Inglês</p>
+						</div>   
+            <div className="knowledge-div">
+							<img src="images/check.png"></img>
+							<p>Português</p>
+						</div>   
+					</details>
+				</article>
+				<div className="introducao-img">
+					<img src="images/undraw_react_y7wq.svg" alt="svg react"></img>
+				</div>
+			</section>
+		</div>
+	);
 }
-

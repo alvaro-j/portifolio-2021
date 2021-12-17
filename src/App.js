d5da1c2b5504e6fc6c6e9234e0103f7d15089358
alvaro-j <alvarojaime360@gmail.com>
2021-12-17 20:42:19 -0300

    change to arrow function

--- src/App.js
+++ src/App.js
@@ -8,73 +8,73 @@ import withReactContent from "sweetalert2-react-content";
 
 const MySwal = withReactContent(Swal);
 
-function App() {
-  return (
-    <Router>
-      <header className="header">
-        <Link to="/">
-          <h1 id="logo">Álvaro</h1>
-        </Link>
-        <nav id="nav">
-          <button id="btn-mobile" onClick={toggleMenu}>
-            <span id="hamburguer"></span>
-          </button>
-          <ul className="menu">
-            <Link to="/contato">
-              <li>Contato</li>
-            </Link>
-            <Link to="/formacao">
-              <li>Formação</li>
-            </Link>
-            <Link to="/projetos">
-              <li>Projetos</li>
-            </Link>
-          </ul>
-        </nav>
-      </header>
+const App = () => {
+	return (
+		<Router>
+			<header className="header">
+				<Link to="/">
+					<h1 id="logo">Álvaro</h1>
+				</Link>
+				<nav id="nav">
+					<button id="btn-mobile" onClick={toggleMenu}>
+						<span id="hamburguer"></span>
+					</button>
+					<ul className="menu">
+						<Link to="/contato">
+							<li>Contato</li>
+						</Link>
+						<Link to="/formacao">
+							<li>Formação</li>
+						</Link>
+						<Link to="/projetos">
+							<li>Projetos</li>
+						</Link>
+					</ul>
+				</nav>
+			</header>
 
-      <Switch>
-        <Route path="/" exact>
-          <TelaHome />
-        </Route>
-        <Route path="/contato">
-          <ContatoTela />
-        </Route>
-        <Route path="/formacao">
-          <FormacaoTela />
-        </Route>
-        <Route path="/projetos">
-          <ProjetosTela />
-        </Route>
-      </Switch>
-    </Router>
-  );
+			<Switch>
+				<Route path="/" exact>
+					<TelaHome />
+				</Route>
+				<Route path="/contato">
+					<ContatoTela />
+				</Route>
+				<Route path="/formacao">
+					<FormacaoTela />
+				</Route>
+				<Route path="/projetos">
+					<ProjetosTela />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 MySwal.fire({
-  position: "top-end",
-  title: "Navegue por aqui &uarr;",
-  showConfirmButton: false,
-  timer: 1500,
-  timerProgressBar: true,
+	position: "top-end",
+	title: "Navegue por aqui &uarr;",
+	showConfirmButton: false,
+	timer: 1500,
+	timerProgressBar: true,
 });
 
-let toggleMenu = () => {
-  const nav = document.getElementById("nav");
-  return nav.classList.toggle("active"); //toggle = add caso não tenha, remova caso tenha
-}
+const toggleMenu = (event) => {
+  const nav = event.currentTarget.parentElement;
+	nav.classList.toggle("active"); //toggle = add caso não tenha, remova caso tenha
+};
 
 function TelaHome() {
-  return <Tela1 />;
+	return <Tela1 />;
 }
 function ContatoTela() {
-  return <Contato />;
+	return <Contato />;
 }
 function FormacaoTela() {
-  return <Formacao />;
+	return <Formacao />;
 }
 function ProjetosTela() {
-  return <Projetos />;
+	return <Projetos />;
 }
 
 export default App;

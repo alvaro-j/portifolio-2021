9dbf7119b4e1db49c55cbf4e9a6e0b64a2054667
alvaro-j <alvarojaime360@gmail.com>
2021-12-21 11:58:33 -0300

    add new project to "projetos"

--- src/components/Projetos.js
+++ src/components/Projetos.js
@@ -1,75 +1,218 @@
-import React from 'react'
+import React from "react";
 
 export default function Projetos() {
-  return (
-			<div>
-					<section className='projetos'>
-							<h1>Projetos:</h1>
-							<article>
-							<div className='projetos-itens'>
-											<img src='images/weather-app.png' alt='weather app'></img>
-											<span>
-											Weather app using OpenWeather API. 
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://weather-app-jet-iota.vercel.app/' target="_blank" rel="noreferrer">Demo</a>
-													<a className='btn' href='http://github.com/alvaro-j/weather-app' target="_blank" rel="noreferrer">Repo</a>
-											</div>
-									</div>
-									<div className='projetos-itens'>
-											<img src='images/buylist.png' alt='buylist'></img>
-											<span>
-											Buy list with add and remove function. 
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://alvaro-j.github.io/buylist-project/'
-													rel='noreferrer' target='_blank'>Demo</a>
-													<a className='btn' href='https://github.com/alvaro-j/buylist-project'
-													rel='noreferrer' target='_blank'>Repo</a>
-											</div>
-									</div>
-									<div className='projetos-itens'>
-											<img src='images/nlw-heat.png' alt='nlw heat'></img>
-											<span>
-											Project developed on NLW Heat using Github API. 
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://alvaro-j.github.io/NLW-Heat/' rel='noreferrer' target='_blank'>Demo</a>
-													<a className='btn' href='https://github.com/alvaro-j/NLW-heat' target='_blank' rel='noreferrer'>Repo</a>
-											</div>
-									</div>
-									<div className='projetos-itens'>
-											<img src='images/pricing-component.png' alt='pricing component'></img>
-											<span>
-											Pricing component with toggle. Challenge from Frontend Mentor. 
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://alvaro-j.github.io/pricing-component/' rel='noreferrer' target='_blank'>Demo</a>
-													<a className='btn' href='https://github.com/alvaro-j/pricing-component' rel='noreferrer' target='_blank'>Repo</a>
-											</div>
-									</div>
-									<div className='projetos-itens'>
-											<img src='images/digital-clock.bmp' alt='digital clock'></img>
-											<span>
-											Digital clock made with HTML, CSS and JavaScript.  
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://alvaro-j.github.io/digital-clock/' rel='noreferrer' target='_blank'>Demo</a>
-													<a className='btn' href='https://github.com/alvaro-j/digital-clock' rel='noreferrer' target='_blank'>Repo</a>
-											</div>
-									</div>
-									<div className='projetos-itens'>
-											<img src='images/tip-calculator.bmp' alt='tip calculator app'></img>
-											<span>
-											Tip calculator app. Challenge from Frondend Mentor. 
-											</span>
-											<div className='container-btn'>
-													<a className='btn' href='https://alvaro-j.github.io/tip-calculator-app/' target='_blank' rel='noreferrer'>Demo</a>
-													<a className='btn' href='http://github.com/alvaro-j/tip-calculator-app' target='_blank' rel='noreferrer'>Repo</a>
-											</div>
-									</div>
-							</article>
-					</section>
-			</div>
-	)
+	return (
+		<div>
+			<section className="projetos">
+				<h1>Projetos:</h1>
+				<article>
+					<div className="projetos-itens">
+						<img src="images/weather-app.png" alt="weather app"></img>
+						<span>Weather app using OpenWeather API.</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://weather-app-jet-iota.vercel.app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="http://github.com/alvaro-j/weather-app"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/nlw-heat.png" alt="nlw heat"></img>
+						<span>Project developed on NLW Heat using Github API.</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/NLW-Heat/"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="https://github.com/alvaro-j/NLW-heat"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/url-shortener.png" alt="url shortener app"></img>
+						<span>
+							URL shortening API landing page. Challenge from Frontend Mentor.
+						</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/url-shortening/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="https://github.com/alvaro-j/url-shortening"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/buylist.png" alt="buylist"></img>
+						<span>Buy list with add and remove function.</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/buylist-project/"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="https://github.com/alvaro-j/buylist-project"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img
+							src="images/pricing-component.png"
+							alt="pricing component"
+						></img>
+						<span>
+							Pricing component with toggle. Challenge from Frontend Mentor.
+						</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/pricing-component/"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="https://github.com/alvaro-j/pricing-component"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/digital-clock.bmp" alt="digital clock"></img>
+						<span>Digital clock made with HTML, CSS and JavaScript.</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/digital-clock/"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="https://github.com/alvaro-j/digital-clock"
+								rel="noreferrer"
+								target="_blank"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/tip-calculator.bmp" alt="tip calculator app"></img>
+						<span>Tip calculator app. Challenge from Frondend Mentor.</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/tip-calculator-app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="http://github.com/alvaro-j/tip-calculator-app"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/interrogacao.svg" alt="interrogação"></img>
+						<span>Soon...</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/tip-calculator-app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="http://github.com/alvaro-j/tip-calculator-app"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+					<div className="projetos-itens">
+						<img src="images/interrogacao.svg" alt="interrogação"></img>
+						<span>Soon...</span>
+						<div className="container-btn">
+							<a
+								className="btn"
+								href="https://alvaro-j.github.io/tip-calculator-app/"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Demo
+							</a>
+							<a
+								className="btn"
+								href="http://github.com/alvaro-j/tip-calculator-app"
+								target="_blank"
+								rel="noreferrer"
+							>
+								Repo
+							</a>
+						</div>
+					</div>
+				</article>
+			</section>
+		</div>
+	);
 }

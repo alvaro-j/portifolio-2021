4fabf2d69994ba5893f47c83cc527967650d827e
alvaro-j <alvarojaime360@gmail.com>
2021-12-24 09:32:33 -0300

    remove header code from app to Header component

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Tela1 from "./components/Tela1";
-import Contato from "./components/Contato";
-import Formacao from "./components/Formacao";
-import Projetos from "./components/Projetos";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
-import { NavLink } from "react-router-dom";
+import Header from "./components/Header";
 
 const MySwal = withReactContent(Swal);
 
 const App = () => {
-	return (
-		<Router>
-			<header className="header">
-				<Link to="/">
-					<h1 id="logo">Álvaro</h1>
-				</Link>
-				<nav id="nav">
-					<button
-						id="btn-mobile"
-						onClick={({ currentTarget }) =>
-							currentTarget.parentElement.classList.toggle("active")
-						}
-					>
-						<span id="hamburguer"></span>
-					</button>
-					<ul className="menu">
-						<NavLink activeClassName="link-active" to="/contato">
-							<li>Contato</li>
-						</NavLink>
-						<NavLink activeClassName="link-active" to="/formacao">
-							<li>Formação</li>
-						</NavLink>
-						<NavLink activeClassName="link-active" to="/projetos">
-							<li>Projetos</li>
-						</NavLink>
-					</ul>
-				</nav>
-			</header>
-
-			<Switch>
-				<Route path="/" exact>
-					<TelaHome />
-				</Route>
-				<Route path="/contato">
-					<ContatoTela />
-				</Route>
-				<Route path="/formacao">
-					<FormacaoTela />
-				</Route>
-				<Route path="/projetos">
-					<ProjetosTela />
-				</Route>
-			</Switch>
-		</Router>
-	);
+	return <Header />;
 };
 
 MySwal.fire({
@@ -64,18 +15,5 @@ MySwal.fire({
 	timer: 1500,
 	timerProgressBar: true,
 });
-
-function TelaHome() {
-	return <Tela1 />;
-}
-function ContatoTela() {
-	return <Contato />;
-}
-function FormacaoTela() {
-	return <Formacao />;
-}
-function ProjetosTela() {
-	return <Projetos />;
-}
 
 export default App;

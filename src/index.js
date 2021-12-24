0335375eeb41c6c4abeb3b8ea539e80650230f54
alvaro-j <alvarojaime360@gmail.com>
2021-12-24 09:39:12 -0300

    add header.scss import

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 import "./styles/global.scss";
+import "./styles/header.scss";
 import "./styles/introducao.scss";
 import "./styles/contato.scss";
 import "./styles/formacao.scss";

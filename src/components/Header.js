fc78bc45fdc0a2d8e764803855223f39774882db
alvaro-j <alvarojaime360@gmail.com>
2021-12-24 10:04:46 -0300

    add hooks useState and useEffect :D

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import Tela1 from "./Tela1"
-import Contato from "./Contato"
-import Formacao from "./Formacao"
-import Projetos from "./Projetos"
-
+import Tela1 from "./Tela1";
+import Contato from "./Contato";
+import Formacao from "./Formacao";
+import Projetos from "./Projetos";
 
 const Header = () => {
+	const [pageTitle, setPageTitle] = React.useState(document.title);
+
+	React.useEffect(() => {
+		document.title = pageTitle; //quando o componento for renderizado pelo setPageTitle, ele executa esse useEffect pra trocar o título da página
+	});
 	return (
 		<Router>
 			<header className="header">
-				<Link to="/">
+				<Link
+					onClick={() => {
+						setPageTitle("Portifólio - Home");
+					}}
+					to="/"
+				>
 					<h1 id="logo">Álvaro</h1>
 				</Link>
 				<nav id="nav">
@@ -24,13 +33,31 @@ const Header = () => {
 						<span id="hamburguer"></span>
 					</button>
 					<ul className="menu">
-						<NavLink activeClassName="link-active" to="/contato">
+						<NavLink
+							onClick={() => {
+								setPageTitle("Portifólio - Contato");
+							}}
+							activeClassName="link-active"
+							to="/contato"
+						>
 							<li>Contato</li>
 						</NavLink>
-						<NavLink activeClassName="link-active" to="/formacao">
+						<NavLink
+							onClick={() => {
+								setPageTitle("Portifólio - Formação");
+							}}
+							activeClassName="link-active"
+							to="/formacao"
+						>
 							<li>Formação</li>
 						</NavLink>
-						<NavLink activeClassName="link-active" to="/projetos">
+						<NavLink
+							onClick={() => {
+								setPageTitle("Portifólio - Projetos");
+							}}
+							activeClassName="link-active"
+							to="/projetos"
+						>
 							<li>Projetos</li>
 						</NavLink>
 					</ul>

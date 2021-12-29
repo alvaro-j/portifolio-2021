d35ff76ae1454ed0b474d50e8e025b7f020961fe
alvaro-j <alvarojaime360@gmail.com>
2021-12-29 09:29:27 -0300

    add hook useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import Header from "./components/Header";
-
-const MySwal = withReactContent(Swal);
+import React from "react";
 
 const App = () => {
+	React.useEffect(() => {
+		const MySwal = withReactContent(Swal);
+		MySwal.fire({
+			position: "top-end",
+			title: "Navegue por aqui &uarr;",
+			showConfirmButton: false,
+			timer: 1500,
+			timerProgressBar: true,
+		});
+	}, [])
 	return <Header />;
 };
 
-MySwal.fire({
-	position: "top-end",
-	title: "Navegue por aqui &uarr;",
-	showConfirmButton: false,
-	timer: 1500,
-	timerProgressBar: true,
-});
+
 
 export default App;

3793b55345092d0593e806ce68ef76854680609f
alvaro-j <alvarojaime360@gmail.com>
2022-02-24 10:34:50 -0300

    translation done

--- src/components/Formacao.js
+++ src/components/Formacao.js
@@ -1,24 +1,21 @@
 import React from "react";
 
+import { Trans } from "react-i18next";
+
 export default function Formacao() {
 	return (
 		<div>
 			<section className="formacao">
 				<article>
-					<h1>Formação</h1>
+					<h1><Trans>Education</Trans></h1>
 					<span className="formacao-span">
-						<img src="/images/if.png" alt="instituto federal"></img> Instituto
-						Federal de Pernambuco
+						<img src="/images/if.png" alt="instituto federal"></img> Instituto Federal de Pernambuco
 					</span>
-					<p>Análise e Desenvolvimento de Sistemas</p>
+					<p><Trans>Analysis and Systems Development</Trans></p>
 					<p>2020-2023</p>
 				</article>
 				<div className="formacao-img">
-					<img
-						draggable="false"
-						src="/images/graduation.svg"
-						alt="graduation svg"
-					></img>
+					<img draggable="false" src="/images/graduation.svg" alt="graduation svg"></img>
 				</div>
 			</section>
 		</div>

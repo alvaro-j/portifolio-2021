9d1a908890c158a434be66d6a6f8f13eb4b92de8
alvaro-j <alvarojaime360@gmail.com>
2022-02-24 10:29:31 -0300

    translation done

--- src/components/Tela1.js
+++ src/components/Tela1.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { Trans } from "react-i18next";
 
 export default function Tela1() {
 	const showDiv = (e) => {
-		e.target.nextElementSibling.classList.toggle("active-know-div")
-		e.target.parentElement.classList.toggle("active-know-div")
-	}
+		e.target.nextElementSibling.classList.toggle("active-know-div");
+		e.target.parentElement.classList.toggle("active-know-div");
+	};
+
 	return (
 		<div>
 			<section className="introducao">
 				<article>
-					<h2>👋Hello world</h2>
+					
+					<h2>
+						👋<Trans>Hello world</Trans>
+					</h2>
 					<h1>Álvaro Jaime</h1>
-					<h2>Front end Developer</h2>
-					<p>Conhecimentos:</p>
+					<h2>
+						<Trans>Front end Developer</Trans>
+					</h2>
+					<p>
+						<Trans>Knowledge</Trans>:
+					</p>
 					<div className="knowledge-wrapper active-know-div">
 						<span onClick={showDiv}>Front end</span>
 						<div className="knowledge-itens active-know-div">
@@ -31,11 +40,13 @@ export default function Tela1() {
 						</div>
 					</div>
 					<div className="knowledge-wrapper">
-						<span onClick={showDiv}>Ferramentas</span>
+						<span onClick={showDiv}>
+							<Trans>Tools</Trans>
+						</span>
 						<div className="knowledge-itens">
 							<div className="knowledge-item">
 								<img src="images/check.png" alt="check icon"></img>
-								<p>Git e Github</p>
+								<p>Git, Github</p>
 							</div>
 							<div className="knowledge-item">
 								<img src="images/check.png" alt="check icon"></img>
@@ -44,25 +55,27 @@ export default function Tela1() {
 						</div>
 					</div>
 					<div className="knowledge-wrapper">
-						<span onClick={showDiv}>Idiomas</span>
+						<span onClick={showDiv}>
+							<Trans>Languages</Trans>
+						</span>
 						<div className="knowledge-itens">
 							<div className="knowledge-item">
 								<img src="images/check.png" alt="check icon"></img>
-								<p>Português fluente</p>
+								<p>
+									<Trans>Fluent Portuguese</Trans>
+								</p>
 							</div>
 							<div className="knowledge-item">
 								<img src="images/check.png" alt="check icon"></img>
-								<p>Inglês avançado</p>
+								<p>
+									<Trans>Advanced English</Trans>
+								</p>
 							</div>
 						</div>
 					</div>
 				</article>
 				<div className="introducao-img">
-					<img
-						draggable="false"
-						src="images/undraw_react_y7wq.svg"
-						alt="svg react"
-					></img>
+					<img draggable="false" src="images/undraw_react_y7wq.svg" alt="svg react"></img>
 				</div>
 			</section>
 		</div>
